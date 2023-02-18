ff32ba90e79cf47982fdf63e9ea45556e5fe23b3
tobyatgithub <toby.fangyuan@gmail.com>
2023-02-18 13:28:52 -0800

    add selected models and parameter logs.

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=C:\Users\tobyf\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=tobytoby
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS="--share"
 git pull
 call webui.bat
